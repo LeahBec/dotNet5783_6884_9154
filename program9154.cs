f88b108daca3a9adc405c81a89bdce17a8a043fc
tamarheisler <tamar8846@outlook.co.il>
2022-09-19 14:31:28 +0300

    add welcome function

--- program9154.cs
+++ program9154.cs
@@ -1,8 +1,16 @@
 ﻿using System;
 
-public class Class1
+partial public class Class1
 {
 	public Class1()
 	{
+		Welcome();
+
 	}
+    private static void Welcome()
+    {
+        Console.WriteLine("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first console application", name);
+    }
 }

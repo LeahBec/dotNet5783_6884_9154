4f8992e9f713cd3224b7b1332e10177ddab651cb
Tamar Hizler <tamarl8846@gmail.com>
2022-11-30 22:40:03 +0200

    update orderforlist

--- project/BL/BO/OrderForList.cs
+++ project/BL/BO/OrderForList.cs
@@ -1,29 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BO
+﻿namespace BO;
+public class OrderForList
 {
-    public class OrderForList
-    {
-        public int ID { get; set; }
-        public string? CustomerName { get; set; }
-        public OrderStatus? Status { get; set; }
-        public int AmountOfItems { get; set; }
-        public double TotalPrice { get; set; }
+    public int ID { get; set; }
+    public string? CustomerName { get; set; }
+    public OrderStatus? Status { get; set; }
+    public int AmountOfItems { get; set; }
+    public double TotalPrice { get; set; }
 
-        public override string ToString()
-        {
-            string order = $@"
-        Order id: {ID}
+    public override string ToString()
+    {
+        string order = $@"
+        Product id: {ID}
         Customer Name: {CustomerName}, 
     	Status : {Status}
         Amount Of Items : {AmountOfItems}
         Total Price : {TotalPrice}";
-            return order;
-        }
-
+        return order;
     }
 }

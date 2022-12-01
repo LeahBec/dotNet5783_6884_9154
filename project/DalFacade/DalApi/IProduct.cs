fa7a7cce5cfe787c2d6f4607fe5b888dae9ce3ee
Tamar Hizler <tamarl8846@gmail.com>
2022-12-01 02:10:34 +0200

    canceling the error

--- project/DalFacade/DalApi/IProduct.cs
+++ project/DalFacade/DalApi/IProduct.cs
@@ -5,4 +5,6 @@ namespace DalApi;
 /// used to implement the additional required functions that are not 
 /// implemented in the base template CRUD interface
 /// </summary>
-public interface IProduct : ICrud<Product> { }
+public interface IProduct : ICrud<Product> {
+    void updateAmount(int a, int b);
+}

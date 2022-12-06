07112017de881bdc824496ae07810bb36dba764f
tamarheisler <tamar8846@outlook.co.il>
2022-12-06 12:15:14 +0200

    without Pl

--- project/DalList/DalList.cs
+++ project/DalList/DalList.cs
@@ -9,8 +9,7 @@ using DalList;
 namespace Dal
 {
     sealed public class DalList : IDal
-    {
-        public IProduct Product => new DalProduct() { };
+    {   public IProduct Product => new DalProduct() { };
         public IOrder Order => new DalOrder() { };
         public IOrderItem OrderItem => new DalOrderItem() { };
     }

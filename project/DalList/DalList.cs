ae9bb497b6ea2f8d3980da5639864bdb15b50923
LeahBec <leah.beckerman@gmail.com>
2022-12-25 02:33:32 +0200

    singleton - not working yet

--- project/DalList/DalList.cs
+++ project/DalList/DalList.cs
@@ -8,11 +8,37 @@ using DalList;
 
 namespace Dal
 {
-    sealed internal class DalList : IDal
-    {   public IProduct Product => new DalProduct() { };
+    sealed internal  class DalList : IDal
+    {
+        //private Lazy<DalList> instance { get; } = new Lazy<DalList>();
+
+        //private static readonly Lazy<DalList> lazy =
+        //new Lazy<DalList>(() => new DalList());
+
+        //public static DalList Instance { get { return lazy.Value; } }
+
+        //private DalList()
+        //{
+        //}
+        static private Lazy<DalList> instance { get; set; } = null;
+        public static IDal Instance { get => GetInstance(); }
+
+        public IProduct Product => new DalProduct() { };
         public IOrder Order => new DalOrder() { };
         public IOrderItem OrderItem => new DalOrderItem() { };
-        public static IDal instance { get; } = new DalList();
-        private DalList() {}
+        private static readonly object padlock = new object();
+
+        private DalList() { }
+        public static DalList GetInstance()
+        {
+            lock (padlock)
+            {
+                if (instance == null)
+                    instance = new Lazy<DalList>();
+
+                return instance.Value;
+            }
+
+        }
     }
 }

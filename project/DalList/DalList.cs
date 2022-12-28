d21e82ff0439a77e8c3ab0355e25935e632a45fb
tamarheisler <tamar8846@outlook.co.il>
2022-12-28 12:52:28 +0200

    finish stage 4 with lazy

--- project/DalList/DalList.cs
+++ project/DalList/DalList.cs
@@ -10,40 +10,24 @@ namespace Dal
 {
     sealed internal class DalList : IDal
     {
-        //private Lazy<DalList> instance { get; } = new Lazy<DalList>();
+       
 
-        //private static readonly Lazy<DalList> lazy =
-        //new Lazy<DalList>(() => new DalList());
-
-        //public static DalList Instance { get { return lazy.Value; } }
-
-        //private DalList()
-        //{
-        //}
-
-        // stack overflow
-        // private static readonly Lazy<DalList> _Instance = 
-        //    new Lazy<DalList>(() => new DalList());
-        //
-
-        static private DalList? instance  = null;
+        static private Lazy<DalList>? instance  = null;
         public static IDal Instance { get => GetInstance(); }
 
         
         public IProduct Product => new DalProduct() { };
         public IOrder Order => new DalOrder() { };
         public IOrderItem OrderItem => new DalOrderItem() { };
-        //private static readonly object padlock = new object();
 
         private DalList() { }
         public static DalList GetInstance()
         {
-            lock (instance??=new DalList () )
+            lock (instance??=new Lazy<DalList> (()=> new DalList()) )
             {
-                //if (instance == null)
-                //    instance = new DalList();
+         
 
-                return instance;//??new DalList();
+                return instance.Value ;
             }
 
         }
